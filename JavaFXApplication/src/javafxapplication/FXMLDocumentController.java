a573c4de5577fb4f610a11869124fb4377e75ca4
j4marcos <joaomarcos2004.jonh@gmail.com>
2024-01-16 15:49:41 -0300

    working with javaFX

---
+++ JavaFXApplication/src/javafxapplication/FXMLDocumentController.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ */
+package javafxapplication;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author j4marcos
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label label;
+    @FXML
+    private Button button;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("handle button action!");
+    }
+    
+    @FXML
+    private void clicked(ActionEvent event) {
+        System.out.println("You clicked");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
